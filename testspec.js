3ccfba6b1c42edd5a09afd0e8c96893c82a9d190
Milenkaclaure <milenka.claure@assuresoft.com>
2017-07-17 14:10:25 -0700

    before page object, working

--- testspec.js
+++ testspec.js
@@ -5,26 +5,34 @@ describe('Protractor Demo App', function() {
     var goButton = element(by.id('gobutton'));
     var latestResult = element(by.binding('latest'));
     var history = element.all(by.repeater('result in memory'));
-
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
     function add(a, b) {
         firstNumber.sendKeys(a);
         secondNumber.sendKeys(b);
         goButton.click();
     }
-    beforeEach(function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+    it('should count all the history', function() {
+        add(1, 2);
+        add(3, 4);
+        add(5, 6);
+        var numberOfElements =  element.all(by.repeater('result in memory')).count();
+        expect(numberOfElements).toEqual(3);
     });
+
 
     it('should have a title', function() {
         expect(browser.getTitle()).toEqual('Super Calculator');
     });
 
-    it('should add one and two', function() {
-        firstNumber.sendKeys(1);
+    it('should reduse two from three', function() {
+        firstNumber.sendKeys(3);
+        element(by.cssContainingText('option', '-')).click();
         secondNumber.sendKeys(2);
         goButton.click();
 
-        expect(latestResult.getText()).toEqual('3');
+        expect(latestResult.getText()).toEqual('1');
     });
 
     it('should add four and six', function() {
@@ -38,11 +46,11 @@ describe('Protractor Demo App', function() {
     it('should have a history', function() {
         add(1, 2);
         add(3, 4);
-        expect(history.count()).toEqual(2);
         add(5, 6);
 
         expect(history.last().getText()).toContain('1 + 2');
     });
+
 
 
 
