e37053e6dc2814b316a83c448ae2dfa8f8c8affc
Milenkaclaure <milenka.claure@assuresoft.com>
2017-07-18 07:22:43 -0700

    page object working, with different directory

---
+++ googletest.js
@@ -0,0 +1,27 @@
+var AngularPage = require('./pages/AngularPage');
+
+describe('angularjs homepage', function () {
+    var page;
+
+    beforeEach(function () {
+        page = new AngularPage();
+    });
+
+    it('should greet the named user', function () {
+        page.typeName('Julie');
+        expect(page.greeting).toEqual('Hello Julie!');
+    });
+
+    describe('todo list', function () {
+        it('should list todos', function () {
+            expect(page.todoList.count()).toEqual(2);
+            expect(page.todoAt(1)).toEqual('build an AngularJS app');
+        });
+
+        it('should add a todo', function () {
+            page.addTodo('write a protractor test');
+            expect(page.todoList.count()).toEqual(3);
+            expect(page.todoAt(2)).toEqual('write a protractor test');
+        });
+    });
+});
--- pageObject.js
+++ pageObject.js
@@ -1,18 +1,19 @@
-
 var HomePage = function() {
-
-
     this.viewOnGitHubButton = element(by.css(".button.button-small.button-plain"));
 
 };
 describe('angularjs.org', function() {
+    //var homePage =  require("./pages/HomePage");
     var homePage = new HomePage();
 
     beforeEach(function() {
         browser.get('https://angularjs.org/');
     });
 
-    it('should have a three buttons', function() {
+    it('should have a github buttons', function() {
         expect(homePage.viewOnGitHubButton.isDisplayed()).toBe(true);
+    });
+    it('should redirect to github', function(){
+        homePage.viewOnGitHubButton.click();
     });
 });
